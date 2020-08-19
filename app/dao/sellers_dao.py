64fb786c67c6505ca88af66480522a681dfeefdd
Paulo Netto <pauloestrella1994@gmail.com>
2020-08-19 11:46:12 -0300

    Sellers.dao constructor

---
+++ app/dao/sellers_dao.py
@@ -0,0 +1,24 @@
+from app.dao.base_dao import BaseDao
+from app.model.sellers_model import Sellers
+
+class SellersDao(BaseDao):
+
+    def __init__(self):
+        self.__table_name = 'sellers'
+        super().__init__(Sellers)
+
+    #read
+    def read(self, id:str =''):
+        return super().read(id)
+
+    #create
+    def create(self, model:Sellers) -> Sellers:
+        model.id = super().insert(model)
+
+    #update
+    def update(self, model:Sellers) -> Sellers:
+        model.id = super().update(model)
+
+    #delete
+    def delete(self, id:str ='') -> dict:
+        return super().delete(id)
